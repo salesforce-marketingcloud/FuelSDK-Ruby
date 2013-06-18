d492b1187c61b216e2de19bed3c96b5046751fac
Michael Clark <michaelallenclark@gmail.com>
2013-06-18 07:51:27 -0400

    Added Fixes from Master for Refresh Token

--- ET_Client.rb
+++ ET_Client.rb
@@ -154,10 +154,11 @@ class ET_Client < ET_CreateWSDL
 				http = Net::HTTP.new(uri.host, uri.port)
 				http.use_ssl = true
 				request = Net::HTTP::Post.new(uri.request_uri)
-				jsonPayload = {'clientId' => @clientId, 'clientSecret' => @clientSecret}
+				jsonPayload = {'clientId' => @clientId, 'clientSecret' => @clientSecret, 'accessType' => 'offline'}
 				#Pass in the refreshKey if we have it
 				if @refreshKey then
 					jsonPayload['refreshToken'] = @refreshKey
+					jsonPayload['scope'] = "cas:#{@internalAuthToken}"
 				end
 
 				request.body = jsonPayload.to_json
