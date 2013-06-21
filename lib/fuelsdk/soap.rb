c4d6d00a30db569d04269e2a1941100e71793616
barberj <barber.justin@gmail.com>
2013-06-21 15:10:36 -0400

    exposed extended properties

--- lib/fuelsdk/soap.rb
+++ lib/fuelsdk/soap.rb
@@ -46,19 +46,30 @@ module FuelSDK
   end
 
   class DescribeResponse < SoapResponse
-    attr_reader :properties, :retrievable, :updatable, :required
+    attr_reader :properties, :retrievable, :updatable, :required, :extended, :viewable, :editable
     private
+
       def unpack_rslts raw
-        @retrievable, @updatable, @required, @properties = [], [], [], [], []
-        rslts = raw.body[raw.body.keys.first][:object_definition][:properties]
-        rslts.each do  |r|
-          @retrievable << r[:name] if r[:is_retrievable]
-          @updatable << r[:name] if r[:is_updatable]
-          @required << r[:name] if r[:is_required]
-          @properties << r[:name]
+        @retrievable, @updatable, @required, @properties, @extended, @viewable, @editable = [], [], [], [], [], [], [], []
+        definition = raw.body[raw.body.keys.first][:object_definition]
+        _props = definition[:properties]
+        _props.each do  |p|
+          @retrievable << p[:name] if p[:is_retrievable]
+          @updatable << p[:name] if p[:is_updatable]
+          @required << p[:name] if p[:is_required]
+          @properties << p[:name]
+        end
+        # ugly, but a necessary evil
+        _exts = definition[:extended_properties].nil? ? {} : definition[:extended_properties] # if they have no extended properties nil is returned
+        _exts = _exts[:extended_property] || [] # if no properties nil and we need an array to iterate
+        _exts = [_exts] unless _exts.kind_of? Array # if they have only one extended property we need to wrap it in array to iterate
+        _exts.each do  |p|
+          @viewable << p[:name] if p[:is_viewable]
+          @editable << p[:name] if p[:is_editable]
+          @extended << p[:name]
         end
         @success = true # overall_status is missing from definition response, so need to set here manually
-        rslts
+        _props + _exts
       rescue
         @message = "Unable to describe #{raw.locals[:message]['DescribeRequests']['ObjectDefinitionRequest']['ObjectType']}"
         @success = false
