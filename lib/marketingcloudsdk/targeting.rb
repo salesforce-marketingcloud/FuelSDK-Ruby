0cc9c6842c485f0362d79659a299f4ba606920c7
Ionut Petre <ipetre@salesforce.com>
2018-08-27 14:04:23 +0300

    Add support for user specific endpoints
    Add 'base_api_url' and 'request_token_url'
    Update Campaign and Asset objects

--- lib/marketingcloudsdk/targeting.rb
+++ lib/marketingcloudsdk/targeting.rb
@@ -50,7 +50,7 @@ module MarketingCloudSDK::Targeting
   protected
     def determine_stack
       options = {'params' => {'access_token' => self.access_token}}
-      response = get("https://www.exacttargetapis.com/platform/v1/endpoints/soap", options)
+      response = get(self.base_api_url +  "/platform/v1/endpoints/soap", options)
       @endpoint = response['url']
     rescue => e
       raise 'Unable to determine stack using: ' + e.message
