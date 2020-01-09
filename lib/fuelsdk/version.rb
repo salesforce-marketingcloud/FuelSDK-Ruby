99418e52486a2563828e10afe21cde3142fdf45a
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-09 13:37:17 +0000

    bump version for new gemstash release

--- lib/fuelsdk/version.rb
+++ lib/fuelsdk/version.rb
@@ -1,5 +1,5 @@
 module FuelSDK
-  base = "0.0.8"
+  base = "0.0.9"
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
