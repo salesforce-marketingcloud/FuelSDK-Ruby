9716f742ea4d82fd8b0ee854df387b382e2dd064
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-09 13:37:17 +0000

    gemstash versioning for dependabot

--- lib/fuelsdk/version.rb
+++ lib/fuelsdk/version.rb
@@ -1,3 +1,6 @@
 module FuelSDK
-  VERSION = "0.0.8"
+  base = "0.0.8"
+
+  # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
+  VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
 end
